b0d73514ffaadf95e9a1911c63033a3e1ed50690
Kevin Christianson <kevinchristianson@eduroam-173-41.dyn.carleton.edu>
2017-03-30 17:15:37 -0500

    added board, mover, move and test files

---
+++ TTT3D/src/edu/carleton/kevinchristianson/TTT3DMoverTest.java
@@ -0,0 +1,33 @@
+package edu.carleton.kevinchristianson;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by kevinchristianson on 3/30/17.
+ */
+class TTT3DMoverTest {
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void winningMoves() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void blockingMoves() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void forcingMoves() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void bestMove() {
+    }
+
+}

13ec9af05be225e96f71a5de2075c823b6dc0dcc
haseleyi <haseim1@hotmail.com>
2017-03-30 19:51:52 -0500

    Added cases for winningMoves

--- TTT3D/src/edu/carleton/kevinchristianson/TTT3DMoverTest.java
+++ TTT3D/src/edu/carleton/kevinchristianson/TTT3DMoverTest.java
@@ -16,6 +16,12 @@ class TTT3DMoverTest {
 
     @org.junit.jupiter.api.Test
     void winningMoves() {
+        /* Cases:
+            Vertical, horizontal, diagonal in 2D
+            Vertical, horizontal, diagonal in 3D
+            One winning move, completes two different 3-in-a-rows
+            Multiple options
+        */
     }
 
     @org.junit.jupiter.api.Test

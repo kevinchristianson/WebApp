a2ec8a0ab25b89b80a45ac60961fd29b6fdf66a6
Kevin Christianson <kevinchristianson@eduroam-173-41.dyn.carleton.edu>
2017-03-30 20:22:04 -0500

    Commented winningMoves method

--- TTT3D/src/edu/carleton/kevinchristianson/TTT3DMoverTest.java
+++ TTT3D/src/edu/carleton/kevinchristianson/TTT3DMoverTest.java
@@ -13,7 +13,10 @@ class TTT3DMoverTest {
     @org.junit.jupiter.api.AfterEach
     void tearDown() {
     }
-
+    /**
+     creates boards with the following criteria and checks that the winningMoves method returns correct options:
+     win by 2D horizontal, 2D vertical, 2D diagonal, 3D vertical, 3D diagonal, only one option, multiple options, intersecting options
+     */
     @org.junit.jupiter.api.Test
     void winningMoves() {
     }

f0b062b8f7b3b3f3268d55d2bf0651cd9812ad80
Kevin Christianson <kevinchristianson@eduroam-173-41.dyn.carleton.edu>
2017-04-22 13:59:49 -0500

    created ap_tests.py

---
+++ WebApp/tests/api_tests.py
@@ -0,0 +1,22 @@
+'''
+api_tests.py
+Isaac Haseley and Kevin Christianson
+
+'''
+
+#import CLASS TO TEST HERE
+import unittest
+
+class APITester(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()

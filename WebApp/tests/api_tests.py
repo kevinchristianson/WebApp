77405f26ac1fbf7b9a633cb36cdb2b1c5ad6fa02
haseleyi <haseim1@hotmail.com>
2017-04-23 12:49:13 -0500

    Start on single college tests

--- WebApp/tests/api_tests.py
+++ WebApp/tests/api_tests.py
@@ -4,17 +4,51 @@ Isaac Haseley and Kevin Christianson
 
 '''
 
-#import CLASS TO TEST HERE
 import unittest
 
 class APITester(unittest.TestCase):
+    def get_college(name):
+        pass
+
     def setUp(self):
         pass
 
     def tearDown(self):
         pass
 
+    def testNormalCaseUnderscoreCaps(self):
+        result = get_college("Carleton_College")
+        expected
 
+    def testNormalCaseUnderscoreLower(self):
+        pass
+
+    def testNormalCaseUnderscoreMixedCaps(self):
+        pass
+
+    def testNormalCaseSpaceCaps(self):
+        pass
+
+    def testNormalCaseSpaceLower(self):
+        pass
+
+    def testNormalCaseSpaceMixedCaps(self):
+        pass
+
+    def testNormalCaseLongName(self):
+        pass
+
+    def testPartialNameOneMatch(self):
+        pass
+
+    def testPartialNameMultipleMatches(self):
+        pass
+
+    def testInvalidName(self):
+        pass
+
+    def testEmptyName(self):
+        pass
 
 
 

c6858233a0a334fdb5569c4df28273457c67e312
haseleyi <haseim1@hotmail.com>
2017-05-07 15:41:25 -0500

    Organization for phase 4

---
+++ WebApp/config.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+'''
+    config.py
+    Kevin Christianson and Isaac Haseley
+    Database login info for our college metrics web app
+'''
+
+# Change these values as appropriate for your postgresql setup.
+database = 'collegedata'
+user = 'Isaac'
+password = ''

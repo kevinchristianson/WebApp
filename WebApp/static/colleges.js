e7bebd92071d0e57bb5e405d2af5845cbc01129e
haseleyi <haseim1@hotmail.com>
2017-05-08 21:40:45 -0500

    Refactored buttons to match Jeff's samples

--- WebApp/static/colleges.js
+++ WebApp/static/colleges.js
@@ -4,3 +4,10 @@
  *
  *  Javascript to support our college metrics web app
  */
+
+ function onCollegeSearchButton() {
+
+ 	// var colorBoxElement = document.getElementById('colorbox');
+  //   var magicWordElement = document.getElementById('magicword');
+  //   colorBoxElement.innerHTML = 'The magic word is "' + magicWordElement.value + '"';
+ }

ec69c87bc6fdc5d02c989357f1ce4c1036b9e4f5
haseleyi <haseim1@hotmail.com>
2017-05-08 22:20:03 -0500

    First button ready for testing

--- WebApp/static/colleges-config.js
+++ WebApp/static/colleges-config.js
@@ -5,5 +5,5 @@
  *  Configuration file for our college metrics web application
  */
 
-// Set this to match the base URL at which your books_api.py is listening.
+// Set this to match the base URL at which your api.py is listening.
 var api_base_url = 'http://localhost:8080/';
--- WebApp/static/colleges.js
+++ WebApp/static/colleges.js
@@ -5,9 +5,32 @@
  *  Javascript to support our college metrics web app
  */
 
- function onCollegeSearchButton() {
+function onCollegeSearchButton() {
+	var collegeSearchElement = document.getElementById('inputcollege');
+	var url = api_base_url + 'schools/search/' + collegeSearchElement;
+	xmlHttpRequest = new XMLHttpRequest();
+	xmlHttpRequest.open('get', url);
+	xmlHttpRequest.onreadystatechange = function() {
+	        if (xmlHttpRequest.readyState == 4 && xmlHttpRequest.status == 200) { 
+	            collegeSearchCallback(xmlHttpRequest.responseText);
+	        } 
+	    }; 
+	xmlHttpRequest.send(null);
+}
 
- 	// var colorBoxElement = document.getElementById('colorbox');
-  //   var magicWordElement = document.getElementById('magicword');
-  //   colorBoxElement.innerHTML = 'The magic word is "' + magicWordElement.value + '"';
- }
+function collegeSearchCallback(responseText) {
+    var collegeList = JSON.parse(responseText);
+    
+    // If responseText is one item, go to that school's page
+
+    var resultsTextElement = document.getElementById('results_text');
+    resultsTableElement.innerHTML = collegeList[0]['name'];
+
+	// Else if it's multiple items, show the helper page with options and links
+	// Else if it's empty, stay and display an error message
+        
+}
+
+function onStateSearchButton() {
+	var stateSearchElement = document.getElementById('inputstate');
+}

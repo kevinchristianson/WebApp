d47d23e0e374ca54241dbbdea07f79078b3f45d4
haseleyi <haseim1@hotmail.com>
2017-05-13 12:32:40 -0500

    Config: Added my thacker creds

--- WebApp/config.py
+++ WebApp/config.py
@@ -17,9 +17,14 @@ else:
     user = 'haseleyi'
     password = ''
 if thacker:
-    database = 'christiansonk'
-    user = 'christiansonk'
-    password = 'carpet784books'
+    if kevin:
+        database = 'christiansonk'
+        user = 'christiansonk'
+        password = 'carpet784books'
+    else:
+        database = 'haseleyi'
+        user = 'haseleyi'
+        password = 'seal522field'
     api_base_url = 'http://thacker.mathcs.carleton.edu:5107/'
     website_base_url = 'http://thacker.mathcs.carleton,edu:5207/'
     images_base_url = 'http://thacker.mathcs.carleton.edu:5116/image?'

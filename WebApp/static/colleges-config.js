9d865d4e0899a4ac229e6b971da556fa2486cf3d
Kevin Christianson <kevinchristianson@res-235-163.dyn.carleton.edu>
2017-05-13 01:05:04 -0500

    fixed config.js urls

--- WebApp/static/colleges-config.js
+++ WebApp/static/colleges-config.js
@@ -8,8 +8,8 @@
 // Set this to match the base URL at which your api.py is listening.
 var thacker = false
 if(thacker){
-    var api_base_url = 'http://thacker.mathcs.carleton.edu:5107'
-    var website_base_url = 'http://thacker.mathcs.carleton.edu:5207'
+    var api_base_url = 'http://thacker.mathcs.carleton.edu:5107/'
+    var website_base_url = 'http://thacker.mathcs.carleton.edu:5207/'
 }else{
     var api_base_url = 'http://localhost:8080/'
     var website_base_url = 'http://localhost:8081/'

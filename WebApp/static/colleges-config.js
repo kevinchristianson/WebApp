3990cfb01b87aede24bb6df41c8c1832001f84f3
Kevin Christianson <kevinchristianson@res-235-163.dyn.carleton.edu>
2017-05-13 01:00:20 -0500

    fixed but in colleges-config.js

--- WebApp/static/colleges-config.js
+++ WebApp/static/colleges-config.js
@@ -9,7 +9,7 @@
 var thacker = false
 if(thacker){
     var api_base_url = 'http://thacker.mathcs.carleton.edu:5107'
-    var website_base_url = 'http://thacker.mathcs.carleton,edu:5207'
+    var website_base_url = 'http://thacker.mathcs.carleton.edu:5207'
 }else{
     var api_base_url = 'http://localhost:8080/'
     var website_base_url = 'http://localhost:8081/'

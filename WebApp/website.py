4a9c19f79185a911ac77bb5bb59dcfb95642c2ac
haseleyi <haseim1@hotmail.com>
2017-05-12 20:15:34 -0500

    Jeff's suggestions

--- WebApp/website.py
+++ WebApp/website.py
@@ -21,34 +21,38 @@ def get_main_page():
 
 @app.route('/schools/search/<search_text>')
 def get_school_search_page(search_text):
+    # Retrieve a college's data from the API
     while ' ' in search_text:
         search_text = search_text[:search_text.index(' ')] + '_' + search_text[search_text.index(' ') + 1:]
     api_url = config.api_base_url + 'schools/search/' + search_text
     data_from_server = urllib.request.urlopen(api_url).read()
     string_from_server = data_from_server.decode('utf-8')
-    data = json.loads(string_from_server)
-    if(len(data) == 1):
-        search_name = data[0]['name'].lower()
+    college_list = json.loads(string_from_server)
+
+    # Retrieve a college's image from Bing's image API
+    if (len(college_list) == 1):
+        search_name = college_list[0]['name'].lower()
         while ' ' in search_name:
             search_name = search_name[:search_name.index(' ')] + '_' + search_name[search_name.index(' ') + 1:]
-        image_url = config.images_base_url + 'name=' + search_name + '&state=' + data[0]['state'].lower()
+        image_url = config.images_base_url + 'name=' + search_name + '&state=' + college_list[0]['state'].lower()
         data_from_image_server = urllib.request.urlopen(image_url).read()
         image_data = json.loads(data_from_image_server.decode('utf-8'))
-        data.append(image_data)
-        return flask.render_template('college_page.html', message = data)
-    return flask.render_template('state_page.html', message = data)
+        college_list.append(image_data)
+        return flask.render_template('college_page.html', message = college_list)
+
+    return flask.render_template('state_page.html', message = college_list)
 
 @app.route('/schools/by_state/<search_text>')
 def get_state_search_page(search_text):
-    # For when we have full-name state search implemented
+    # Retrieve a state's schools from the API
     while ' ' in search_text:
         search_text = search_text[:search_text.index(' ')] + '_' + search_text[search_text.index(' ') + 1:]
     api_url = config.api_base_url + 'schools/by_state/' + search_text
     data_from_server = urllib.request.urlopen(api_url).read()
     string_from_server = data_from_server.decode('utf-8')
-    data = [search_text.upper()]
-    data.append(json.loads(string_from_server))
-    return flask.render_template('state_page.html', message = data)
+    college_list = [search_text.upper()]
+    college_list.append(json.loads(string_from_server))
+    return flask.render_template('state_page.html', message = college_list)
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:

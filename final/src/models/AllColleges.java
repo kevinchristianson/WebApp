f7b4da03aebe7aead4feb59cc7efd7a1199a892f
haseleyi <isaac.haseley@gmail.com>
2017-05-28 18:19:09 -0500

    Changes

--- final/src/models/AllColleges.java
+++ final/src/models/AllColleges.java
@@ -1,9 +1,25 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Kevin Christianson and Isaac Haseley
  *
  * Data structure to handle all Colleges that we build from our API
  */
 public class AllColleges {
+
+    private List CollegeList;
+
+    /**
+     * Hits the API and constructs all Colleges
+     * Sticks them all in CollegeList
+     */
+    public AllColleges() {
+    }
+
+    public List<College> getCollegeList() {
+        return null;
+    }
 }
--- final/src/models/College.java
+++ final/src/models/College.java
@@ -1,9 +1,70 @@
 package models;
 
+import java.util.HashMap;
+
 /**
  * Kevin Christianson and Isaac Haseley
+ *
+ * Handles data on a single college or university from our API
  */
 public class College {
 
+    /**
+     * How well this institution performs given the user's metrics
+     */
+    private double totalWeightedOutcome;
 
+    /**
+     * Public, private, or for-profit
+     */
+    private String designation;
+
+    private double acceptanecRate;
+    private int inStateTuition, outStateTuition, midpointACT, enrollment;
+    private String state;
+
+    /**
+     * Constructs College with API data
+     * @param apiDictionary - dictionary returned from our API
+     */
+    public College(HashMap apiDictionary) {
+    }
+
+    /**
+     * @param userWeights - Dictionary with key for each metric and value for user's weight
+     */
+    public void calcTotalWeightedOutcome(HashMap userWeights) {
+    }
+
+    public double getTotalWeightedOutcome() {
+        return totalWeightedOutcome;
+    }
+
+    public double getAcceptanecRate() {
+        return acceptanecRate;
+    }
+
+    public int getInStateTuition() {
+        return inStateTuition;
+    }
+
+    public int getOutStateTuition() {
+        return outStateTuition;
+    }
+
+    public int getMidpointACT() {
+        return midpointACT;
+    }
+
+    public int getEnrollment() {
+        return enrollment;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public String getState() {
+        return state;
+    }
 }

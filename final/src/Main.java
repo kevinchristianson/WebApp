626bcc2d8b906db55722c50af4d407dadf3cbf63
haseleyi <isaac.haseley@gmail.com>
2017-06-01 17:50:20 -0500

    JSON Integration

--- final/src/Main.java
+++ final/src/Main.java
@@ -1,11 +1,18 @@
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import java.io.*;
-import java.net.*;
-import java.util.Map;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Main extends Application {
@@ -19,29 +26,24 @@ public class Main extends Application {
     }
 
     public static void iCanHasInternat() throws IOException {
-//        URL url = new URL("http://thacker.mathcs.carleton.edu:5107/schools/search/carleton_college");
-//        BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
-//        String inputLine;
-//        Map<String, String> myMap = new HashMap<>();
-//        String[] pairs = inputLine.split(",");
-//        for (int i = 0; i < pairs.length; i++) {
-//            String pair = pairs[i]
-//            String[] keyValue = pair.split(":");
-//            myMap.put(keyValue[0], Integer.valueOf(keyValue[1]));
-//        }
-////        while ((inputLine = in.readLine()) != null)
-////            System.out.println(inputLine);
-//        in.close();
-//        JSONParser parser = new JSONParser();
+        URL url = new URL("http://thacker.mathcs.carleton.edu:5107/schools/search/carleton_college");
+        BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+        String apiOutput = in.readLine();
+
+        JsonParser myParser = new JsonParser();
+        JsonElement myElement = myParser.parse(apiOutput);
+        JsonArray myArray = myElement.getAsJsonArray();
+        JsonObject myObject = myArray.get(0).getAsJsonObject();
+        System.out.println(myObject.get("name"));
 
     }
 
     public static void main(String[] args) {
-//        try {
-//            iCanHasInternat();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-        launch(args);
+        try {
+            iCanHasInternat();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+//        launch(args);
     }
 }

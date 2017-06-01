9b1966d7f97ac4559a260fc8445f6da046dd785b
haseleyi <isaac.haseley@gmail.com>
2017-05-31 21:10:51 -0500

    Reads URL to String

--- final/src/sample/Main.java
+++ final/src/sample/Main.java
@@ -5,6 +5,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import java.io.*;
+import java.net.*;
 
 public class Main extends Application {
 
@@ -16,8 +18,21 @@ public class Main extends Application {
         primaryStage.show();
     }
 
+    public static void iCanHasInternat() throws IOException {
+        URL url = new URL("http://thacker.mathcs.carleton.edu:5107/schools/search/carleton_college");
+        BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+        String inputLine;
+        while ((inputLine = in.readLine()) != null)
+            System.out.println(inputLine);
+        in.close();
+    }
 
     public static void main(String[] args) {
+        try {
+            iCanHasInternat();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         launch(args);
     }
 }
